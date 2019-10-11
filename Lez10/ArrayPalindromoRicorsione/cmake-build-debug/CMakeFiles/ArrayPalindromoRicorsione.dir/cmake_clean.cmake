e09b4b1435224818ae3a310e23930921912754ed
AndreaDagg <daguannoandrea@outlook.com>
2019-10-11 16:46:04 +0200

    Lezione 1010

---
+++ Lez10/ArrayPalindromoRicorsione/cmake-build-debug/CMakeFiles/ArrayPalindromoRicorsione.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ArrayPalindromoRicorsione.exe"
+  "ArrayPalindromoRicorsione.exe.manifest"
+  "ArrayPalindromoRicorsione.pdb"
+  "CMakeFiles/ArrayPalindromoRicorsione.dir/main.c.obj"
+  "libArrayPalindromoRicorsione.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArrayPalindromoRicorsione.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
