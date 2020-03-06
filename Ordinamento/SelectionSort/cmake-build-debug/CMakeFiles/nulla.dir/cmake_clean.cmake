96ebb938a09b0126bd97a16bc62b6826dbf313a2
AndreaDagg <daguannoandrea@outlook.com>
2020-03-06 21:56:46 +0100

    SelectionSort

---
+++ Ordinamento/SelectionSort/cmake-build-debug/CMakeFiles/nulla.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nulla.dir/main.c.obj"
+  "libnulla.dll.a"
+  "nulla.exe"
+  "nulla.exe.manifest"
+  "nulla.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nulla.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
