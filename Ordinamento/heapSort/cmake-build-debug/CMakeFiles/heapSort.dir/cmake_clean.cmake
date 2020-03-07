5479be2186b7750f57d3ad133ba2a539867bf729
AndreaDagg <daguannoandrea@outlook.com>
2020-03-07 15:12:28 +0100

    heapsort

---
+++ Ordinamento/heapSort/cmake-build-debug/CMakeFiles/heapSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heapSort.dir/main.c.obj"
+  "heapSort.exe"
+  "heapSort.exe.manifest"
+  "heapSort.pdb"
+  "libheapSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/heapSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
