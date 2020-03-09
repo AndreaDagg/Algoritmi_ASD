6e8d440fe97eb7e12e70b1ce856c830ee7af66a0
AndreaDagg <daguannoandrea@outlook.com>
2020-03-09 12:14:47 +0100

    quick

---
+++ Ordinamento/quickSort/cmake-build-debug/CMakeFiles/quickSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quickSort.dir/main.c.obj"
+  "libquickSort.dll.a"
+  "quickSort.exe"
+  "quickSort.exe.manifest"
+  "quickSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quickSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
