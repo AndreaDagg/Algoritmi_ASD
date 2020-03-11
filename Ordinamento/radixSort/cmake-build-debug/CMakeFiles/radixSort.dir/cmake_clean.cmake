843616863dab402e71ed061aefea673abb7f6e0a
AndreaDagg <daguannoandrea@outlook.com>
2020-03-11 17:28:36 +0100

    countingSor

---
+++ Ordinamento/radixSort/cmake-build-debug/CMakeFiles/radixSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radixSort.dir/main.c.obj"
+  "libradixSort.dll.a"
+  "radixSort.exe"
+  "radixSort.exe.manifest"
+  "radixSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/radixSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
