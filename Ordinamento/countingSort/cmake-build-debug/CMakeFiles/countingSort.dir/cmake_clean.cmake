92cef0be76e8dc3bc42caf9ffa39711ae868e49f
AndreaDagg <daguannoandrea@outlook.com>
2020-03-11 17:26:22 +0100

    countingSort

---
+++ Ordinamento/countingSort/cmake-build-debug/CMakeFiles/countingSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/countingSort.dir/main.c.obj"
+  "countingSort.exe"
+  "countingSort.exe.manifest"
+  "countingSort.pdb"
+  "libcountingSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/countingSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
