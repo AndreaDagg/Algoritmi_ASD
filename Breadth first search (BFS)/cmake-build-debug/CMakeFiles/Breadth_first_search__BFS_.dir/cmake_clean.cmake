c3f826d5267ef7cd0b61f55f71ccad5372f8ac71
AndreaDagg <daguannoandrea@outlook.com>
2020-04-09 17:44:56 +0200

    Bfs

---
+++ Breadth first search (BFS)/cmake-build-debug/CMakeFiles/Breadth_first_search__BFS_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Breadth_first_search__BFS_.exe"
+  "Breadth_first_search__BFS_.exe.manifest"
+  "Breadth_first_search__BFS_.pdb"
+  "CMakeFiles/Breadth_first_search__BFS_.dir/main.c.obj"
+  "libBreadth_first_search__BFS_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Breadth_first_search__BFS_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
