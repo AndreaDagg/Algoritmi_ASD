312e4587d4a5c0b3fb251c9d4b4d217a7b353511
AndreaDagg <daguannoandrea@outlook.com>
2020-04-09 17:52:00 +0200

    Dfs

---
+++ Graph and Tree Algorithms/Deep frist searc (DFS)/cmake-build-debug/CMakeFiles/Deep_frist_searc__DFS_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Deep_frist_searc__DFS_.dir/main.c.obj"
+  "Deep_frist_searc__DFS_.exe"
+  "Deep_frist_searc__DFS_.exe.manifest"
+  "Deep_frist_searc__DFS_.pdb"
+  "libDeep_frist_searc__DFS_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Deep_frist_searc__DFS_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Graph and Tree Algorithms/Deep frist searc (DFS)/cmake-build-debug/CMakeFiles/Deep_frist_searc__DFS_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Deep_frist_searc__DFS_.
+# This may be replaced when dependencies are built.
