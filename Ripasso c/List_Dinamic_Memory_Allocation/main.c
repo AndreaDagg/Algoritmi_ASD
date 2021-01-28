47faaf7bc0a0ed0eea688aa6c7b8c42bd87bcfa9
AndreaDagg <daguannoandrea@outlook.com>
2021-01-28 23:00:07 +0100

    i_C

--- Ripasso c/List_Dinamic_Memory_Allocation/main.c
+++ Ripasso c/List_Dinamic_Memory_Allocation/main.c
@@ -13,14 +13,38 @@ typedef struct node List;
 
 List *createList(const int *numNodPtr);
 
+int count_List(List *headPTR);
+
+void insert_node(const int *positionPtr, List *headerPTR);
+
 void leggiLista(List *nodePtr);
+
+List *delete_node(List *NodePtr, const int *nodeToDelete);
 
 int main() {
 
     List *listPtr = NULL;
-    int numNodi = 3;
+    int numNodi = 3, insert;
 
     listPtr = createList(&numNodi);
+    leggiLista(listPtr);
+
+    int cout = count_List(listPtr);
+    printf("\nLa lsita ha %d elementi\n", cout);
+
+    do {
+        printf("Dove inserire il nodo? da 0 a %d -> ", cout+1);
+        scanf("%d", &insert);
+    } while (insert < 0 || insert > cout+1);
+    insert_node(&insert, listPtr);
+    leggiLista(listPtr);
+
+    cout = count_List(listPtr);
+    do {
+        printf("Dove elimino il nodo? da 0 a %d -> ", cout);
+        scanf("%d", &insert);
+    } while (insert < 0 || insert > cout);
+    listPtr = delete_node(listPtr, &insert);
     leggiLista(listPtr);
 
 
@@ -70,11 +94,81 @@ List *createList(const int *numNodPtr) {            //Funzione che ritorina l'in
 void leggiLista(List *nodePtr) {
     printf("\n");
 
-    int nodNum = 1;
+    int nodNum = 0;
     while (nodePtr != NULL) {
         printf("Il valore del nodo %d -> %d\n", nodNum, nodePtr->payload);
         nodNum++;
         nodePtr = nodePtr->next;
     }
 
+}
+
+void insert_node(const int *positionPtr, List *headerPTR) {
+    int countNode = 0;
+    List *precedenteNodo = malloc(sizeof(List));
+    if (*positionPtr == 0) {
+
+        List *newNode = malloc(sizeof(List));
+        newNode->payload = 1000 + *positionPtr;
+        newNode->next = headerPTR;
+        headerPTR = newNode;
+        leggiLista(headerPTR);
+    } else {
+
+        while (headerPTR != NULL && countNode != *positionPtr) {
+
+            precedenteNodo = headerPTR;
+            headerPTR = headerPTR->next;
+            countNode++;
+
+        }
+        if (countNode == *positionPtr) {
+            List *newNode = malloc(sizeof(List));
+            newNode->payload = 1000 + *positionPtr;
+
+            precedenteNodo->next = newNode;
+            newNode->next = headerPTR;
+
+        } else {
+            //Non esiste la posizione nella lista
+        }
+
+    }
+}
+
+List *delete_node(List *headPtr, const int *nodeToDelete) {
+
+    int coutNode = 0;
+    List *PrecNodePtr;
+    List *tempoNode = headPtr;
+
+    while (tempoNode != NULL && *nodeToDelete != coutNode) {
+        PrecNodePtr = tempoNode;
+        tempoNode = tempoNode->next;
+        coutNode++;
+    }
+
+    if (*nodeToDelete == 0) {
+        headPtr = tempoNode->next;
+        free(tempoNode);
+        return headPtr;
+    } else if (coutNode == *nodeToDelete) {
+        tempoNode = PrecNodePtr->next;
+        PrecNodePtr->next = PrecNodePtr->next->next;
+        free(tempoNode);
+        return headPtr;
+
+    }
+
+    //   return NodePtr;
+}
+
+int count_List(List *headPTR) {
+    int c = -1;
+    while (headPTR != NULL) {
+        headPTR = headPTR->next;
+        c++;
+    }
+
+    return c;
 }
