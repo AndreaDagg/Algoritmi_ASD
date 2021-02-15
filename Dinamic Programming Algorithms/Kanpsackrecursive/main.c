d0bcecd84e56000048c85ec5dc3e12036f16130f
AndreaDagg <daguannoandrea@outlook.com>
2021-02-15 22:21:37 +0100

    zainorecursive

--- Dinamic Programming Algorithms/Kanpsackrecursive/main.c
+++ Dinamic Programming Algorithms/Kanpsackrecursive/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int MAX(int Value1, int Value2);
+int MAX(const int *Value1, const int *Value2);
 
-int ZainoRicorsivo(int w[], int p[], int i, int c, int DP[][c + 1]);
+int ZainoRicorsivo(int w[], int p[], int i, int c, int DP[i+1][c + 1]);
 
-void printMatrix(const int n, const int knappackCapacity, int DP[][knappackCapacity + 1]);
+void printMatrix(int n, int knappackCapacity, int DP[][knappackCapacity + 1]);
 
 int main() {
     int profit[] = {10, 7, 8, 6};
@@ -32,8 +32,8 @@ int main() {
     return 0;
 }
 
-int ZainoRicorsivo(int w[], int p[], int i, int c, int DP[][c + 1]) {
-    printMatrix(i, c, DP);
+int ZainoRicorsivo(int w[], int p[], int i, int c, int DP[i+1][c + 1]) {
+    //printMatrix(i, c, DP);
 
     if (c < 0) {
         return -2;
@@ -42,23 +42,23 @@ int ZainoRicorsivo(int w[], int p[], int i, int c, int DP[][c + 1]) {
     } else {
         if (DP[i][c] < 0) {
             int notTaken = ZainoRicorsivo(w, p, i - 1, c, DP);
-            int Taken = ZainoRicorsivo(w, p, i - 1, c - w[i], DP) + p[i];
-            DP[i][c] = MAX(notTaken, Taken);
+            int Taken = ZainoRicorsivo(w, p, i - 1, c - w[i+1], DP) + p[i+1];
+            DP[i][c] = MAX(&notTaken, &Taken);
         }
         return DP[i][c];
     }
 }
 
 
-int MAX(int Value1, int Value2) {
-    if (Value1 > Value2) {
-        return Value1;
+int MAX(const int *Value1, const int *Value2) {
+    if (*Value1 > *Value2) {
+        return *Value1;
     } else {
-        return Value2;
+        return *Value2;
     }
 }
 
-void printMatrix(const int n, const int knappackCapacity, int DP[][knappackCapacity + 1]) {
+void printMatrix( int n,  int knappackCapacity, int DP[][knappackCapacity + 1]) {
     int row, columns;
     /** itera su 10 colonne */
     for (row = 0; row <= n; row++) {
