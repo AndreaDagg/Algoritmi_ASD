afd3b1cc4cbd3b87361893217c6d8aee2fe15c17
AndreaDagg <daguannoandrea@outlook.com>
2021-03-17 22:05:31 +0100

    RECLIST

---
+++ #3_CONCATENATE RECURSION TWO LISTS/cmake-build-debug/CMakeFiles/_3_RECURSION.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_3_RECURSION.dir/main.c.obj"
+  "_3_RECURSION.exe"
+  "_3_RECURSION.exe.manifest"
+  "_3_RECURSION.pdb"
+  "lib_3_RECURSION.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_3_RECURSION.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
